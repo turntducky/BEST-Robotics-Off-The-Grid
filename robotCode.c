c989e6552d15a275c809769c087ab94eba6ffe27
Turnt Ducky <turntducky@gmail.com>
2019-10-25 11:11:40 -0500

    Update robotCode.c

--- robotCode.c
+++ robotCode.c
@@ -156,11 +156,11 @@ task main()
 			else if (getIrReading(rightIR) == 1) //If right IR is over black
 			{ //This lets the autonomous correct if robot is over to much to the right
 				startMotor(leftMotor, 63); //Left motor will stay at value 63
-				startMotor(rightMotor, -63); //Right motor will stop to allow correction
+				startMotor(rightMotor, -63); //Right motor will reverse to allow correction
 			}
 			else if (getIrReading(leftIR) == 1) //If left IR is over black
 			{ //This lets the autonomous correct if robot is over to much to the left
-				startMotor(leftMotor, -63); //Left motor will stop to allow correction
+				startMotor(leftMotor, -63); //Left motor will reverse to allow correction
 				startMotor(rightMotor, 63); //Right motor will stay at value 63
 			}
 			if(SensorValue [LimitSwitch] == 1) //If limit switch is pressed in
